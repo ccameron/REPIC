8eff24f23a0f3f37a31a242e86c68f936999e1a8
Christopher JF Cameron <ccameron@Christophers-MacBook-Pro.local>
2024-06-24 10:45:06 -0400

    resub updates

--- setup.py
+++ setup.py
@@ -11,7 +11,9 @@ from setuptools import find_packages, setup
 
 # package meta-data
 NAME = "repic"
-DESCRIPTION = "REPIC - an ensemble learning approach for cryo-EM particle picking."
+DESCRIPTION = (
+    "REPIC - a consensus methodology for harnessing multiple cryo-EM particle pickers."
+)
 URL = "https://github.com/ccameron/REPIC"
 EMAIL = "christopher.cameron@yale.edu"
 AUTHOR = "Christopher JF Cameron"
@@ -25,7 +27,7 @@ REQUIRED = [
     "numpy>=1.24.2",
     "pandas>=2.0.2",
     "scipy>=1.10.0",
-    "tqdm>=4.65.0"
+    "tqdm>=4.65.0",
 ]
 
 work_dir = os.path.abspath(os.path.dirname(__file__))
@@ -33,13 +35,13 @@ work_dir = os.path.abspath(os.path.dirname(__file__))
 # import README and use as the long-description
 try:
     with open(os.path.join(work_dir, "README.md"), encoding="utf-8") as f:
-        long_description = '\n' + f.read()
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
 # load the package's __version__.py module as a dictionary
 about = {}
-project_slug = NAME.lower().replace('-', '_').replace(' ', '_')
+project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
 with open(os.path.join(work_dir, project_slug, "__version__.py")) as f:
     exec(f.read(), about)
 
@@ -57,24 +59,29 @@ setup(
     include_package_data=True,
     data_files=[
         ("docs", ["docs/cryolo.md", "docs/deeppicker.md", "docs/topaz.md"]),
-        ("docs/patches/deeppicker", ["docs/patches/deeppicker/autoPick.py",
-                                     "docs/patches/deeppicker/autoPicker.py",
-                                     "docs/patches/deeppicker/dataLoader.py",
-                                     "docs/patches/deeppicker/deepModel.py",
-                                     "docs/patches/deeppicker/starReader.py",
-                                     "docs/patches/deeppicker/train.py"])
+        (
+            "docs/patches/deeppicker",
+            [
+                "docs/patches/deeppicker/autoPick.py",
+                "docs/patches/deeppicker/autoPicker.py",
+                "docs/patches/deeppicker/dataLoader.py",
+                "docs/patches/deeppicker/deepModel.py",
+                "docs/patches/deeppicker/starReader.py",
+                "docs/patches/deeppicker/train.py",
+            ],
+        ),
     ],
     entry_points={
         "console_scripts": ["repic=repic.main:main"],
     },
     install_requires=REQUIRED,
-    license='BSD-3-Clause',
+    license="BSD-3-Clause",
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy"
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
 )
